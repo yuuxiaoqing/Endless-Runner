699b84389fdf8c5f0648642e37efd2d4611cf7e2
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-04-28 02:27:30 -0700

    hover effect works on one button: menu button at endgame scene

--- src/scenes/Endgame.js
+++ src/scenes/Endgame.js
@@ -14,22 +14,26 @@ class Endgame extends Phaser.Scene{
         this.add.text(width/2, (height/2)-40, "end scene(score, high score)").setOrigin(0.5);
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
-        this.menu = this.add.text(width/2, height/2+100, ">menu<").setOrigin(0.5);
-        this.menu.setInteractive().on('pointerdown', ()=>{
+        //from https://snowbillr.github.io/blog/2018-07-03-buttons-in-phaser-3/
+        this.menu = this.add.text(width/2, height/2+100, ">menu<", {fill: '#fff'}).setOrigin(0.5)
 
-            this.scene.start('menuScene');
-
-        });
-
+        .setInteractive()
+        .on('pointerdown', ()=>{this.menu.setStyle({fill: '#fa0'}); this.scene.start('menuScene'); })
+        .on('pointerover', ()=>{this.menu.setStyle({fill:'#fa0'});})
+        .on('pointerout', ()=>{this.menu.setStyle({fill:'#fff'})});
+       
         this.restart = this.add.text(width/2, height/2, ">restart<").setOrigin(0.5);
+    
         this.restart.setInteractive().on('pointerdown',()=>{
 
             this.scene.start('playScene');
 
-        })
+        });
     }
     update(){
 
+       
+       
     }
 
 }
