594684390e18e01ad81828745d0b24be1326a44d
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-04-28 19:33:32 -0700

    basic obstacle prefab

---
+++ src/prefabs/Obstacle.js
@@ -0,0 +1,16 @@
+class Obstacle extends Phaser.GameObjects.Sprite{
+
+    constructor(scene, x, y, texture, frame){
+         super(scene, x, y, texture, frame);
+         scene.add.existing(this);
+    }
+
+    update(){
+        
+
+    }
+
+
+
+
+}
