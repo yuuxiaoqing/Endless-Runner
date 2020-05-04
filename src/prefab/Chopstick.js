4852fe6b2991a4b1b6e5a21ac00ae2e156fdf4a7
Xiao Qing Yu <yuuxiaoqing@gmail.com>
2020-05-03 22:23:30 -0700

    most of the assets added

--- src/prefab/Chopstick.js
+++ src/prefab/Chopstick.js
@@ -3,7 +3,7 @@ class Chopstick extends Phaser.Physics.Arcade.Sprite{
     //inspired from Professor's paddle barrier code, edited to work for this game.
     constructor(scene, speed){
         //tweek to generate chopstick only on the table
-        super(scene,Phaser.Math.Between(width-30, 30), height+90,'temp');
+        super(scene,Phaser.Math.Between(width-30, 30), height+90,'chopstick');
         scene.add.existing(this);
 
         scene.physics.add.existing(this); //add physics chopstick to scene
@@ -11,17 +11,19 @@ class Chopstick extends Phaser.Physics.Arcade.Sprite{
         this.setVelocityY(speed); //make the chopsticks go up
         this.setImmovable();
         this.newChopstick = true;
+
         
   
 
     }
 
-
     update(){
 
         super.update();
+        
         if(this.newChopstick && this.y < (height/2)){
             this.newChopstick = false;
+            
             this.scene.addChopstick(this.parent, this.speed);
             
         }
